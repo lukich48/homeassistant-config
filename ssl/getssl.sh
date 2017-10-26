e0cc1f41051c342ae268d6a6c282a5d556f1921d
Rizjiy <rodal2807@gmail.com>
2017-10-27 01:38:22 +0300

    recorder fix

--- ssl/getssl.sh
+++ ssl/getssl.sh
@@ -1,4 +1,4 @@
 scp -r root@192.168.88.2:/usr/syno/etc/certificate/system/default/\{fullchain.pem,privkey.pem\} /home/homeassistant/.homeassistant/ssl/
-chmod -R 777 /home/homeassistant/.homeassistant/ssl/fullchain.pem
-chmod -R 777 /home/homeassistant/.homeassistant/ssl/privkey.pem
+#chmod -R 777 /home/homeassistant/.homeassistant/ssl/fullchain.pem
+#chmod -R 777 /home/homeassistant/.homeassistant/ssl/privkey.pem
 
