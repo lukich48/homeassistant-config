54d0286c10231816388b0612798a6dce88d4a78a
Rizjiy <rodal2807@gmail.com>
2020-04-10 11:40:56 +0300

    update

--- custom_components/yandex_smart_home/capability.py
+++ custom_components/yandex_smart_home/capability.py
@@ -241,14 +241,24 @@ class OnOffCapability(_Capability):
                                             blocking=self.state.domain != script.DOMAIN, context=data.context)
 
 
-@register_capability
-class ToggleCapability(_Capability):
-    """Mute and unmute functionality.
-
-    https://yandex.ru/dev/dialogs/alice/doc/smart-home/concepts/toggle-docpage/
-    """
+class _ToggleCapability(_Capability):
+    """Base toggle functionality.
+        https://yandex.ru/dev/dialogs/alice/doc/smart-home/concepts/toggle-docpage/
+        """
 
     type = CAPABILITIES_TOGGLE
+
+    def parameters(self):
+        """Return parameters for a devices request."""
+        return {
+            'instance': self.instance
+        }
+
+
+@register_capability
+class MuteCapability(_ToggleCapability):
+    """Mute and unmute functionality."""
+
     instance = 'mute'
 
     @staticmethod
@@ -256,12 +266,6 @@ class ToggleCapability(_Capability):
         """Test if state is supported."""
         return domain == media_player.DOMAIN and features & \
             media_player.SUPPORT_VOLUME_MUTE
-
-    def parameters(self):
-        """Return parameters for a devices request."""
-        return {
-            'instance': self.instance
-        }
 
     def get_value(self):
         """Return the state value of this capability for this entity."""
@@ -284,6 +288,40 @@ class ToggleCapability(_Capability):
             SERVICE_VOLUME_MUTE, {
                 ATTR_ENTITY_ID: self.state.entity_id,
                 media_player.ATTR_MEDIA_VOLUME_MUTED: state['value']
+            }, blocking=True, context=data.context)
+
+
+@register_capability
+class PauseCapability(_ToggleCapability):
+    """Pause and unpause functionality."""
+
+    instance = 'pause'
+
+    @staticmethod
+    def supported(domain, features, entity_config, attributes):
+        """Test if state is supported."""
+        return domain == media_player.DOMAIN \
+            and features & media_player.SUPPORT_PAUSE \
+            and features & media_player.SUPPORT_PLAY
+
+    def get_value(self):
+        """Return the state value of this capability for this entity."""
+        return bool(self.state.state != media_player.STATE_PLAYING)
+
+    async def set_state(self, data, state):
+        """Set device state."""
+        if type(state['value']) is not bool:
+            raise SmartHomeError(ERR_INVALID_VALUE, "Value is not boolean")
+
+        if state['value']:
+            service = media_player.SERVICE_MEDIA_PAUSE
+        else:
+            service = media_player.SERVICE_MEDIA_PLAY
+
+        await self.hass.services.async_call(
+            self.state.domain,
+            service, {
+                ATTR_ENTITY_ID: self.state.entity_id
             }, blocking=True, context=data.context)
 
 
@@ -355,6 +393,75 @@ class ThermostatCapability(_ModeCapability):
             climate.SERVICE_SET_HVAC_MODE, {
                 ATTR_ENTITY_ID: self.state.entity_id,
                 climate.ATTR_HVAC_MODE: value
+            }, blocking=True, context=data.context)
+
+
+@register_capability
+class InputSourceCapability(_ModeCapability):
+    """Input Source functionality"""
+
+    instance = 'input_source'
+
+    media_player_source_map = {
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten'
+    }
+
+    @staticmethod
+    def supported(domain, features, entity_config, attributes):
+        """Test if state is supported."""
+        return domain == media_player.DOMAIN \
+            and features & media_player.SUPPORT_SELECT_SOURCE \
+            and attributes.get(media_player.ATTR_INPUT_SOURCE_LIST) is not None
+
+    def parameters(self):
+        """Return parameters for a devices request."""
+        source_list = self.state.attributes.get(media_player.ATTR_INPUT_SOURCE_LIST)
+        modes = []
+        if source_list is not None:
+            for i in range(0, min(len(source_list), 10)):
+                modes.append({'value': self.media_player_source_map[i + 1]})
+
+        return {
+            'instance': self.instance,
+            'modes': modes,
+            'ordered': True
+        }
+
+    def get_value(self):
+        """Return the state value of this capability for this entity."""
+        source = self.state.attributes.get(media_player.ATTR_INPUT_SOURCE)
+        source_list = self.state.attributes.get(media_player.ATTR_INPUT_SOURCE_LIST)
+        if source_list is not None and source in source_list:
+            return self.media_player_source_map[min(source_list.index(source) + 1, 10)]
+        return self.media_player_source_map[1]
+
+    async def set_state(self, data, state):
+        """Set device state."""
+        value = None
+        source_list = self.state.attributes.get(media_player.ATTR_INPUT_SOURCE_LIST)
+        for index, yandex_value in self.media_player_source_map.items():
+            if yandex_value == state['value']:
+                if index <= len(source_list):
+                    value = source_list[index - 1]
+                break
+
+        if value is None:
+            raise SmartHomeError(ERR_INVALID_VALUE, "Unacceptable value")
+
+        await self.hass.services.async_call(
+            media_player.DOMAIN,
+            media_player.SERVICE_SELECT_SOURCE, {
+                ATTR_ENTITY_ID: self.state.entity_id,
+                media_player.const.ATTR_INPUT_SOURCE: value,
             }, blocking=True, context=data.context)
 
 
